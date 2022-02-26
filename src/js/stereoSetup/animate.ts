16a46ff6865d4c5bbc0fd2c99aac8e0ca77a2149
Zack Cheng <zack_cheng@brown.edu>
2022-02-25 19:15:49 -0500

    removed fps counter, updated build

--- src/js/stereoSetup/animate.ts
+++ src/js/stereoSetup/animate.ts
@@ -1,22 +1,22 @@
-import Stats from 'three/examples/jsm/libs/stats.module'
+// import Stats from 'three/examples/jsm/libs/stats.module'
 import { DemoType } from './main'
 import { updateScene, updateSize } from './update'
 
 // @ts-ignore
-const stats = new Stats()
-stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom)
-stats.dom.style.zIndex = '100000000000000'
+// const stats = new Stats()
+// stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+// document.body.appendChild(stats.dom)
+// stats.dom.style.zIndex = '100000000000000'
 
 export function animate(time: DOMHighResTimeStamp, demo: DemoType, forceRender?: boolean) {
   // Continue the animation loop
   demo.nextFrameReq = requestAnimationFrame((t) => animate(t, demo))
 
-  stats.begin()
+  // stats.begin()
   updateSize(time, demo)
   updateScene(time, demo)
   render(demo)
-  stats.end()
+  // stats.end()
 }
 
 export function render(demo: DemoType): void {
